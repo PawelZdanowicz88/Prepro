84193bc6443365f674e68ef4d49ec24d20dd0242
PawelZdanowicz88 <zdanowiczpawel88@gmail.com>
2022-12-31 16:28:19 +0100

    another files added

---
+++ preprocessor/preprocessor.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    int value1 = 0;
+    int value2 = 0;
+
+#define MODE 1
+
+#if !defined(MODE)
+#error undefined.
+#endif
+#if MODE 0
+std::cout << "Работаю в режиме тренировки";
+#endif
+#if MODE 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> value1;
+    std::cout << "Введите число 2: ";
+    std::cin >> value2;
+    std::cout << "Результат сложения: " << add(value1, value2);
+
+#else 
+    std::cout << "Неизвестный режим. Завершение работы";
+#endif
+
+
+    return 0;
+}
